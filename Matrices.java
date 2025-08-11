86fb1c0bae1c8e50d66e8749a283ed5857a8425b
Uriel Campaña <urielcamp@gmail.com>
2025-08-11 11:59:19 -0300

    semana 1 Matrices: ejercicio 2

--- Matrices.java
+++ Matrices.java
@@ -19,15 +19,15 @@ public class Matrices {
         //1. Crear una matriz 3x3 y mostrarla.
         //Declara una matriz de enteros 3x3, asigna valores manualmente y muéstrala por pantalla.
 
-        int[][] m = {
+        int[][] mUno = {
             {1, 2, 3},
             {4, 5, 6},
             {7, 8, 9}
         };
         
-        for(int i = 0; i < m.length; i++){
-            for(int j = 0; j < m[i].length; j++){
-                System.out.print(m[i][j] + " ");
+        for(int i = 0; i < mUno.length; i++){
+            for(int j = 0; j < mUno[i].length; j++){
+                System.out.print(mUno[i][j] + " ");
             }
             System.out.println();
         }
@@ -46,6 +46,18 @@ public class Matrices {
         System.out.println();
         System.out.println();
 
+        //Matriz 2x4 con todos los valores en 1.
+        //Usa bucles para llenarla y mostrarla.
+
+        int[][] mDos = new int[2][4];
+
+        for(int i = 0; i < mDos.length; i++){
+            for(int j = 0; j < mDos[i].length; j++){
+                mDos[i][j] = 1;
+                System.out.print(mDos[i][j]);
+            }
+            System.out.println();
+        }
 
     }
 }
